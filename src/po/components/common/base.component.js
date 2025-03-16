3cd03fae9f743d74f822533e9b3f3750814ef0ea
Ingrid Munera <ingridmunera24@gmail.com>
2025-03-15 22:25:39 -0500

    commented lines are removed

--- src/po/components/common/base.component.js
+++ src/po/components/common/base.component.js
@@ -6,9 +6,5 @@ class BaseComponent {
     get rootElement() {
         return $(this.rootSelector);
     }
-
-    // item(parentElement, options, option) {
-    //     return parentElement.$(options[option.toLowerCase()]);
-    // }
 }
 module.exports = BaseComponent;
